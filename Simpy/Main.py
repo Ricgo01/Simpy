00b19c5b243f2d3a1a06932c1ba8595c679f261c
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-03-03 12:39:38 -0600

    Funciones
    Se ha creado funciones necesarias para correr el programa

--- Simpy/Main.py
+++ Simpy/Main.py
@@ -2,3 +2,18 @@ import simpy
 import random
 import statistics
 import csv
+
+def ejecutar_simulacion(NUM_PROCESOS, INTERVALO_GENERACION, CAPACIDAD_RAM, VELOCIDAD_CPU, CAPACIDAD_CPU, TIEMPO_SIMULACION):
+    env = simpy.Environment()
+    RAM = simpy.Container(env, init=CAPACIDAD_RAM, capacity=CAPACIDAD_RAM)
+    CPU = simpy.Resource(env, capacity=CAPACIDAD_CPU)
+    tiempos_totales = []
+
+    def proceso(env, nombre, memoria_req, instrucciones, RAM, CPU):
+        tiempo_llegada = env.now
+        yield RAM.get(memoria_req)
+        with CPU.request() as req:
+            yield req
+            yield env.timeout(instrucciones / VELOCIDAD_CPU)
+        yield RAM.put(memoria_req)
+        tiempos_totales.append(env.now - tiempo_llegada)

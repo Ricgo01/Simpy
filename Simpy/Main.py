28ce2e0dc66bc95961a3f7aa5952f823df612eb1
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-03-03 12:47:22 -0600

    Creacion de csv
    Se creo un csv para que asi se puedan guardar los datos de las simulaciones y asi poder hacer las graficas

--- Simpy/Main.py
+++ Simpy/Main.py
@@ -18,14 +18,14 @@ def ejecutar_simulacion(NUM_PROCESOS, INTERVALO_GENERACION, CAPACIDAD_RAM, VELOC
         yield RAM.put(memoria_req)
         tiempos_totales.append(env.now - tiempo_llegada)
 
-def generador_procesos(env, NUM_PROCESOS, INTERVALO_GENERACION, RAM, CPU):
+    def generador_procesos(env, NUM_PROCESOS, INTERVALO_GENERACION, RAM, CPU):
         for i in range(NUM_PROCESOS):
             tiempo_espera = random.expovariate(1.0 / INTERVALO_GENERACION)
             yield env.timeout(tiempo_espera)
             memoria_req = random.randint(1, 10)
             instrucciones = random.randint(1, 20)
             env.process(proceso(env, f'Proceso {i}', memoria_req, instrucciones, RAM, CPU))
-
+    
     env.process(generador_procesos(env, NUM_PROCESOS, INTERVALO_GENERACION, RAM, CPU))
     env.run(until=TIEMPO_SIMULACION)
 
@@ -39,3 +39,14 @@ VELOCIDAD_CPU = 3
 CAPACIDAD_CPU = 2
 TIEMPO_SIMULACION = 1000
 resultados = [("Num Procesos", "Tiempo Promedio")]
+
+for NUM_PROCESOS in [25, 50, 100, 150, 200]:
+    promedio = ejecutar_simulacion(NUM_PROCESOS, INTERVALO_GENERACION, CAPACIDAD_RAM, VELOCIDAD_CPU, CAPACIDAD_CPU, TIEMPO_SIMULACION)
+    resultados.append((NUM_PROCESOS, promedio))
+
+
+with open("resultados_simulacion.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(resultados)
+
+print("Resultados exportados a resultados_simulacion.csv")
